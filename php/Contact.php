d2c604a5fb677fce9178677f8d0df8381648061a
Todd Abraham <tjabraham12@yahoo.com>
2023-09-27 09:39:32 -0500

    Upload Contact.php and ContactStyle.css

---
+++ php/Contact.php
@@ -0,0 +1,52 @@
+<?php include "Navigation_Bar.php" ?>
+<html>
+<head>
+     <!--Links Contact.html to CSS File-->
+     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/Contactstyle.css">
+
+    <title>Contact Us</title>
+</head>   
+<body>
+    <!--Add Line breaks to account for Navigation Bar-->
+    <br><br><br><br><br>
+    <!--Header for Contact Page-->
+    <h1> Contact Us </h1>
+
+<main class="Box-List">
+    <!--Boxes for Contact-->
+    <section class="Contact-box">
+        <img src="tbd.jpg" alt="Phone Icon">
+        <h2>Phone</h2>
+        <p>(309) 712-3385</p>
+    </section>
+    <section class="Contact-box">
+        <img src="tbd.jpg" alt="Email Icon">
+        <h2>Email</h2>
+        <p>info@peaceofheavenpetcare.com</p>
+    </section>
+    <section class="Contact-box">
+        <img src="tbd.jpg" alt="Location Icon">
+        <h2>Location</h2>
+        <p>Located on US-136, 2.5 miles from Interstate 74 <br>
+        Address: 30614 E 200 N Rd, Le Roy, IL 61752
+    </p>
+    </section>
+    <section class="Contact-box">
+        <img src="tbd.jpg" alt="Hours Icon Icon">
+        <h2>Business Hours</h2>
+        <p>Monday-Friday: 6:30am-6:30pm</p>
+    </section>
+</main>
+
+    <h3>Our Social Media</h3>
+    <div class="Social-Media-Images">
+        <img src="Facebook Icon" alt="Facebook Icon">
+        <img src="Instagram Icon" alt ="Instagram Icon">
+        <img src="YouTube Icon" alt="Youtube Icon">
+    </div>
+
+</body> 
+</html>
+<?php include "PoHPC Footer.html" ?>
