7117f9ad31d2c11526df566dee2cc745e6df5ea7
quickliam <quickliam@gmail.com>
2023-09-30 11:32:10 -0500

    Create FAQ.php

---
+++ php/FAQ.php
@@ -0,0 +1,18 @@
+<?php include "Navigation_Bar.php" ?>
+<html>
+<head>
+     <!--Links FAQ.html to CSS File-->
+     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/PeaceOfHeavenWebPage/css/FAQ.css">
+
+    <title>FAQ</title>
+</head>   
+<body>
+    <!--Add Line breaks to account for Navigation Bar-->
+    <br><br><br><br><br>
+
+
+</body> 
+</html>
+<?php include "Footer.php" ?>
