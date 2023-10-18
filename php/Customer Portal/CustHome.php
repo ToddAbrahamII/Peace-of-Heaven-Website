09cf217c6fd337952abf314cb8f6334c49578850
Todd Abraham <tjabraham12@yahoo.com>
2023-10-18 15:19:28 -0500

    Fix Login Bug, Add cust, emp, and adm portal files

--- php/Customer Portal/CustHome.php
+++ php/Customer Portal/CustHome.php
@@ -1,8 +1,8 @@
 <?php
 session_start(); //Starts the session -- REQUIRED ON EVERY PAGE --
 
-    include("connection.php"); //Needed for making login required, calls other php page
-    include("functions.php");//Needed for making login required, calls other php page
+    require("/xampp/htdocs/PeaceOfHeavenWebPage/php/Login/connection.php"); //Needed for making login required, calls other php page
+    require("/xampp/htdocs/PeaceOfHeavenWebPage/php/Login/functions.php");//Needed for making login required, calls other php page
 
     $user_data = check_login($connection); //Needed for making login required, checks credentials
 
@@ -17,7 +17,6 @@ session_start(); //Starts the session -- REQUIRED ON EVERY PAGE --
     <a href="logout.php">Logout</a>
     <h1> Welcome to the Customer Portal </h1>
     <p>Hello, <?php echo $user_data['User_Name']; ?>! </p>
-    
-    
+ 
 </body>
 </html>
