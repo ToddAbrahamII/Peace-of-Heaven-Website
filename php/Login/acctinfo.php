396470ff82420e6a185081317e2e83119d4bc33a
Todd Abraham <tjabraham12@yahoo.com>
2023-10-22 11:47:31 -0500

    Add html section to account info

---
+++ php/Login/acctinfo.php
@@ -0,0 +1,10 @@
+<?php
+session_start(); //Starts the session -- REQUIRED ON EVERY PAGE --
+
+    include("connection.php"); //Needed for making login required, calls other php page
+    include("functions.php");//Needed for making login required, calls other php page
+?>
+<!DOCTYPE html>
+<html>
+    
+</html>
