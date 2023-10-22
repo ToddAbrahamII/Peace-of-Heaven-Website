fcd17a7072ee77e1a9bb5bad93ddf6f5a970bbdd
Todd Abraham <tjabraham12@yahoo.com>
2023-10-22 11:57:36 -0500

    Add Form Folder and Form Php files

---
+++ php/Forms/BoardingForm.php
@@ -0,0 +1,6 @@
+<?php
+session_start(); //Starts the session -- REQUIRED ON EVERY PAGE --
+
+    include("connection.php"); //Needed for making login required, calls other php page
+    include("functions.php");//Needed for making login required, calls other php page
+?>
