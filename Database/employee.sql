cd0aa23cb2181fa73b35ee39b510a0e6ef4695e1
Todd Abraham <tjabraham12@yahoo.com>
2023-10-22 21:07:58 -0500

    Add Database Tables for Cust, Emp,  & Family

---
+++ Database/employee.sql
@@ -0,0 +1,79 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 23, 2023 at 04:04 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `peaceofheavendb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `Emp_ID` bigint(20) NOT NULL,
+  `EmpFirstName` varchar(100) NOT NULL,
+  `EmpLastName` varchar(100) NOT NULL,
+  `EmpPhone` varchar(100) NOT NULL,
+  `EmpAddress` varchar(100) NOT NULL,
+  `EmpState` varchar(100) NOT NULL,
+  `EmpZip` varchar(100) NOT NULL,
+  `User_ID` bigint(20) NOT NULL,
+  `AcctEmail` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`Emp_ID`),
+  ADD KEY `Employee - UserId` (`User_ID`),
+  ADD KEY `Employee - AcctEmail` (`AcctEmail`),
+  ADD KEY `Emp_ID` (`Emp_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `Emp_ID` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `employee`
+--
+ALTER TABLE `employee`
+  ADD CONSTRAINT `Employee - AcctEmail` FOREIGN KEY (`AcctEmail`) REFERENCES `login` (`AcctEmail`),
+  ADD CONSTRAINT `Employee - UserId` FOREIGN KEY (`User_ID`) REFERENCES `login` (`User_ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
