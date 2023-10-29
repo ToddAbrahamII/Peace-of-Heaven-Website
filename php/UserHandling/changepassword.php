528011034ecc61f7991d1a514a790b6b1324ee02
jboogers <jlones@ilstu.edu>
2023-10-28 19:52:07 -0500

    Update changepassword.php
    Added ability for user to change their own password with changepassword.php

--- php/UserHandling/changepassword.php
+++ php/UserHandling/changepassword.php
@@ -9,7 +9,45 @@ if(!$user->isLoggedIn()) {
 
 if(Input::exists()) {
     if(Token::check(Input::get('token'))) {
-        echo 'OK!';
+        $validate = new Validate();
+        $validation = $validate->check($_POST, array(
+            'password_current' =>array(
+                'required'=> true,
+                'min'=> 6,
+                'max'=> 64
+            ),
+            'password_new'=>array(
+                'required'=> true,
+                'min'=> 6,
+                'max'=> 64
+            ),
+            'password_new_again'=>array(
+                'required'=> true,
+                'min'=> 6,
+                'max'=> 64,
+                'matches'=> 'password_new'
+            )
+        ));
+
+        if($validation->passed()) {
+            // change password
+            if(Hash::make(Input::get('password_current'), $user->data()->salt) !== $user->data()->password){
+                echo 'Your current password is wrong.';
+            } else { // passwords match
+                $salt = Hash::salt(32);
+                $user->update(array(
+                    'password' => Hash::make(Input::get('password_new'), $salt),
+                    'salt' => $salt
+                ));
+
+                Session::flash('home','Your password has been changed!');
+                Redirect::to('index.php');
+            }
+        } else {
+            foreach($validation->errors() as $error) {
+                echo $error, '<br>';
+            }
+        }
     }
 }
 ?>
@@ -17,17 +55,17 @@ if(Input::exists()) {
 <form action="" method="post">
     <div class="field">
         <label for="password_current">Current Password</label>
-        <input type="text" name="password_current" id="password_current">
+        <input type="password" name="password_current" id="password_current">
     </div>
 
     <div class="field">
         <label for="password_new">New Password</label>
-        <input type="text" name="password_new" id="password_new">
+        <input type="password" name="password_new" id="password_new">
     </div>
 
     <div class="field">
         <label for="password_new_again">New Password Again</label>
-        <input type="text" name="password_new_again" id="password_new_again">
+        <input type="password" name="password_new_again" id="password_new_again">
     </div>
 
     <input type="submit" value="Change">
