162597e9c89663c6b5b69c39b08f0025758ea6ce
quickliam <quickliam@gmail.com>
2023-10-30 16:09:34 -0500

    Update timeslot.sql

--- Database/timeslot.sql
+++ Database/timeslot.sql
@@ -29,6 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `timeslot` (
   `TimeSlot` date NOT NULL,
+  `IsAvailable` boolean,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
