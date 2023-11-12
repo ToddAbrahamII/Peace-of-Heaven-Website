aa8ca347cf356bdb3c8eec51d7faa00563360374
quickliam <quickliam@gmail.com>
2023-11-12 14:00:31 -0600

    Boarding
    First Draft of Boarding w/ just a copied style sheet

---
+++ php/WebPages/Boarding.php
@@ -0,0 +1,72 @@
+<?php include "Navigation_Bar.php" ?>
+<html>
+<head>
+     <!--Links Contact.html to CSS File-->
+     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/PeaceOfHeavenWebPage/css/Boardingstyle.css">
+
+    <title>Boarding</title>
+</head>   
+<body>
+<!--Create Spacing for the Navigation Bar-->
+
+
+<!--Background image with text-->
+    <div class="Header-container">
+        <h1>Boarding</h1>
+    </div>
+    
+<!--Section for Day Care Image and Description-->
+    <div class = "Clean-Days-container">
+    <img src="/PeaceOfHeavenWebPage/Img/Dog-Portrait.jpg" alt="kennels" Class="Portrait-img">
+    <div class="Clean-Days-Description">
+        <h2>Boarding? Or a Doggy Retreat?</h2>
+        <ul>
+            <li>POH Boarding areas were created and designed to promote a calm environment and provide comfort for dogs of all kinds. </li>
+            <li>Each overnight kennel is a climate-controlled 8x8ft indoor space featuring raised Kuranda bed, food & water bowls, 
+                TVs for familiar sounds of home, windows for sunlight, & staff-monitored cameras for safety.</li>
+            <li>Overnight kennels also have access to individual 8X10ft outdoor yards.</li>
+            <li>Facility space also includes 4800 sq ft indoor exercise & play area and 6000 sq ft outdoor exercise & play areas for individual or social activities</li>
+        </ul>  
+    </div>  
+    </div>
+
+    <!--Space in the Page-->
+    <p><br><br></p>
+
+    <!--Section for Playground Images and Description-->
+    <div class = "Relax-container">
+    <div class="Relax-Description">
+        <h2>Know Before You Go</h2>
+        <p> 
+            <h3> HOURS </h3>
+                Boarding available year-round.
+                Drop-off any time during open hours M-F or by appt on weekends & holidays.
+                Pick-up is available 7 days a week during open hours M-F or by appt on weekends & holidays.
+                Expected pick-up is by 1pm on departure day.  Late pick-up available for additional fee.
+            <h3> PRICING</h3>
+                Boarding Rate…………$75 per night
+                Late Pick-up…………Additional $45 (day care fee); pick-up between 1pm-6:30pm
+                Extended Stays (15+ days)…………Call for availability & pricing
+                <ul>
+                Package Pricing
+                    <li> 10% off total price for 5 or more nights</li>
+                    <li> 10% off daily rate for 2nd dog of same family</li>
+                    <li> Call for pricing & space availability for 3 or more same-family dogs</li>
+                </ul>
+            <h3> BOARDING REQUIREMENTS </h3>
+                Pups must be 6 months or older.
+                Proof of current rabies, distemper-parvo, & bordatella vaccines required.
+                All dogs should be on heartworm & flea/tick preventatives.
+                Dogs must be spayed or neutered to engage in social play with other dogs.</p>
+    </div>  
+    <img src="/PeaceOfHeavenWebPage/Img/dog-relaxing.jpg" alt="kennels" Class="Relax-img">
+    </div>
+    
+    <!--Space in the page-->
+    <p><br><br></p>
+
+</body>
+</html>
+<?php include "Footer.php" ?>
