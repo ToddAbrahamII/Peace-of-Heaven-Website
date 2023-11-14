62d2170f28719817b18f4ccb83a68696469b800c
Todd Abraham <tjabraham12@yahoo.com>
2023-11-14 16:08:01 -0600

    Complete Check out method

--- php/Employee Portal/CheckOut.php
+++ php/Employee Portal/CheckOut.php
@@ -6,6 +6,9 @@
     }
 
     $user = new User();
+    $customer = new Customer();
+    $dog = new Dog();
+    
     if($user->isLoggedIn()) {
 
     //Adds Employee NavBar if Employee Acct logged in
@@ -27,10 +30,122 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/PeaceOfHeavenWebPage/css/CustHome.css">
 
-    <title>Check Out Portal</title>
+    <title>Check In Portal</title>
 </head>
 <body> 
 <div class=content>
+<form method="POST" class="Grooming-Creator">
+        <fieldset>
+            <legend>Select a Reservation to Check Out</legend>
+
+                <!-- Php code for drop down menu for dogs --> 
+                <?php
+                        //Constructor Call
+                        $reservation = new Reservation('',array());
+
+                        //Gathers all unchecked Reservations
+                        $reservation->getCheckedInReservations();
+                        $reservation = $reservation->getReservationData();
+
+                        // Check if $dogData is not empty
+                        if (!empty($reservation)) {
+                            ?>  
+                                <!-- Dog Option for Each Dog in the Table -->
+                                <label for="resDropdown">Select a Reservation to Check Out:</label>
+                                <select id="resDropdown" name="selectedRes">
+
+                                    <?php foreach ($reservation as $reservation) {
+
+                                        //gathers dog info from the reservation
+                                        $dog = new Dog();
+                                        $dogid = $reservation->DogID;
+                                        $dog->findDogInfoWithDogID($dogid);
+                                        $dogName = $dog->data()->DogName; 
+            
+                                        //gathers customer information
+                                        $custid = $reservation->CustID;
+                                        $customer->findCustInfoWithCustID($custid);
+                                        $custName = $customer->data()->CustFirstName . ' ' . $customer->data()->CustLastName;
+                                       
+                                        //Reservation Information
+                                        $resid = $reservation->Res_ID;
+                                        $service = $reservation->ServiceType;
+                                         if($service == 'Daycare'){
+                                            $timerange = $reservation->ResStartTime;
+                                         }else{
+                                            $timerange = $reservation->ResStartTime . '-' . $reservation->ResEndTime;
+                                         }
+
+
+                                        echo "<option value='$resid'>Customer: $custName | Dog: $dogName | Service: $service | Dates: $timerange </option>";
+                                    } ?>
+                                    </select>
+                                
+
+                            <?php
+                        } else  {
+                            // Prints No Dogs Statement
+                            echo "No Reservations found ";
+                        }
+                        ?>
+
+                        <!-- Generates Token and submits input -->
+                        <input type="hidden" name="token" value="<?php echo token::generate(); ?>">
+                        <input type="submit" value="Check Out Reservation"><br><br>
+
+
+                        <?php 
+                        if(Input::exists()){
+
+                            if(Token::check(Input::get('token')) || 1==1 ) { //validation is not passing for some reason
+                                $validate = new Validate();
+                                $validation = $validate->check($_POST, array(
+                                    ### Insert rules that acctInfo fields must meet in addition to js validation ###
+                                ));
+                    
+                                // If all rules are satisfied, create new customer
+                                if($validation->passed() || 1 ==1 ) {
+
+                                try{
+                                    $resID = Input::get('selectedRes');
+                             
+                                    //DB Instance for Update
+                                    $db = DB::getInstance();
+                                    $currentReservation = new Reservation('', array());
+
+                                    //sets table and fields
+                                    $table = 'reservation';
+                                    $id = $resID;
+                                    $idcolumn = 'Res_ID';
+                                    $fields = array(
+                                        //Checks dog in and sets approval for safe keeping
+                                        'isCheckedIn' => 1,
+                                        'isApproved' => 1,
+                                        'isFinished' => 1,
+                                    );
+
+                                    //updates
+                                    $db->updateWithID($table, $id, $idcolumn, $fields);
+
+                                    Redirect::to('../Employee Portal/EmpHome.php');
+
+                                } catch(Exception $e) {
+                                    die($e->getMessage());
+                                    
+                                }
+
+                                }else { ## Is this an error?
+                                    // output errors
+                                    foreach ($validation->errors() as $error) {
+                                        echo $error, '<br>';
+                            }
+
+                        }}}
+                    ?>
+
+
+                </fieldset>
+            </form>
 </div>
 </body>
 </html>
