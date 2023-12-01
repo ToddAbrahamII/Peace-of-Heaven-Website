960f9c3ff77b27f7832b4f2c1c4fb2b5132626d6
quickliam <quickliam@gmail.com>
2023-12-01 11:21:17 -0600

    FAQ
    Updated the style of the FAQ page to match the rest of the front end pages

--- php/WebPages/FAQ.php
+++ php/WebPages/FAQ.php
@@ -5,24 +5,28 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="/PeaceOfHeavenWebPage/css/FAQ.css">
-
         <title>FAQ</title>
     </head>   
+    
     <body>
-        <h1>Frequently Asked Questions:</h1>
-        <p>
-            <h2>Question 1</h2>
-            <div>Answer</div>
-        </p>
-        <p>
-            <h2>Question 2</h2>
-            <div>Answer</div>
-        </p>
-        <p>
-            <h2>Question 3</h2>
-            <div>Answer</div>
-        </p>
-
+    <div class="Header-container">
+        <h1>Frequently Asked Questions</h1>
+    </div>
+    <div class="FAQ-container">
+        <h2>FAQ's:</h2>
+        <div>
+            <h3>Question 1</h3>
+            <p>Answer</p>
+        </div>
+        <div>
+            <h3>Question 1</h3>
+            <p>Answer</p>
+        </div>
+        <div>
+            <h3>Question 1</h3>
+            <p>Answer</p>
+        </div>
+    </div>
     </body> 
 </html>
 <?php include "Footer.php" ?>
